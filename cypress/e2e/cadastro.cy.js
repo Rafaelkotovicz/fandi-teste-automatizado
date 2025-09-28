3b7df9e0b8d8a56caab6ca896b4ddcca6ed5619d
Rafael Kotovicz <rafaelrck21@gmail.com>
2025-09-28 14:37:37 -0300

    adicionando teste de cancelamento de cadastro

---
+++ cypress/e2e/cadastro.cy.js
@@ -0,0 +1,85 @@
+const { it } = require("mocha");
+
+describe('Página de cadastro e gerenciamento', () => {
+    //sempre é executado antes do teste caso haja mais de um teste que precise do acesso a essa página
+    beforeEach(() => {
+        cy.visit('https://www.provaqa.fandi.com.br/');
+        cy.get('button, .btn btn-popup').click();
+    });
+
+    context('Quando o usuário preecher o formulário de cadastro corretamente', () => {
+        it('um novo modelo deve ser cadastrado', () => {
+            // Abrir formulário de cadastro
+            cy.contains('#botaoNovoVeiculo').click();
+
+            // Preencher os campos necessários para o cadastro
+            cy.get('#novaFamilia').select('ASTRA HATCH');
+            cy.get('#novoFabricante').select('CHEVROLET');
+            cy.get('input[name="molicar"]').type('12345');
+            cy.get('input[name="modelo"]').type('ASTRA');
+            cy.get('select[name="combustivel"]').select('Gasolina');
+            cy.get('input[name="portas"]').type('4');
+            cy.get('input[name="anoInicio"]').type('2000');
+            cy.get('input[name="anoFim"]').type('2025');
+            cy.get('input[name="cilindradas"]').type('1400');
+            cy.get('input[name="cavalos"]').type('110');
+
+            // Salvar
+            cy.contains('#btnSalvar').click();
+
+            // Verificar mensagem de sucesso
+            cy.contains('Modelo Cadastrado com Sucesso').should('be.visible');
+        });
+    });
+
+    
+    context('Quando o usuário tentar realizar um cadastro sem preencher os campos obrigatórios', () => {
+        it('Não deve permitir salvar sem preencher os campos obrigatórios', () => {
+            // Abrir formulário de cadastro
+            cy.contains('#botaoNovoVeiculo').click();
+
+            // Tentar salvar sem preencher nada
+            cy.contains('#btnSalvar').click();
+
+            // Validar mensagens de erro nos campos obrigatórios
+            cy.get('#novaFamilia').parent().should('contain', 'Campo Obrigatório.');
+            cy.get('#novoFabricante').parent().should('contain', 'Campo Obrigatório.');
+            cy.get('input[name="modelo"]').parent().should('contain', 'Campo Obrigatório.');
+            cy.get('input[name="molicar"]').parent().should('contain', 'Campo Obrigatório.');
+            cy.get('select[name="combustivel"]').parent().should('contain', 'Campo Obrigatório.');
+            cy.get('input[name="portas"]').parent().should('contain', 'Campo Obrigatório.');
+            cy.get('input[name="anoInicio"]').parent().should('contain', 'Campo Obrigatório.');
+            cy.get('input[name="anoFim"]').parent().should('contain', 'Campo Obrigatório.');
+            cy.get('input[name="cilindradas"]').parent().should('contain', 'Campo Obrigatório.');
+            cy.get('input[name="cavalos"]').parent().should('contain', 'Campo Obrigatório.');
+        });
+
+    });
+    
+
+    context('Quando o usuário preecher o formulário de cadastro e clicar no botão cancelar', () => {
+        it('O cadastro não deve ser realizado', () => {
+            // Abrir o formulário
+            cy.contains('#botaoNovoVeiculo').click();
+
+            // Preencher os campos necessários para o cadastro
+            cy.get('#novaFamilia').select('ASTRA HATCH');
+            cy.get('#novoFabricante').select('CHEVROLET');
+            cy.get('input[name="molicar"]').type('12345');
+            cy.get('input[name="modelo"]').type('ASTRA');
+            cy.get('select[name="combustivel"]').select('Gasolina');
+            cy.get('input[name="portas"]').type('4');
+            cy.get('input[name="anoInicio"]').type('2000');
+            cy.get('input[name="anoFim"]').type('2025');
+            cy.get('input[name="cilindradas"]').type('1400');
+            cy.get('input[name="cavalos"]').type('110');
+
+            // Clicar em Cancelar
+            cy.contains('#btnCancelar').click()
+
+            // Verificar que o formulário foi fechado
+            cy.get('.modal-content-wrapper').should('not.exist')
+        });
+    });
+});
+
